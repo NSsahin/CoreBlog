41a53cb6469455cbea34b910d39dcafbe5c21de1
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-07-24 02:54:00 +0300

    classes-relations

---
+++ DataAccessLayer/Migrations/20230723235138_mig_relations.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_relations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BlogID",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryID",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_BlogID",
+                table: "Comments",
+                column: "BlogID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_CategoryID",
+                table: "Blogs",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Blogs_BlogID",
+                table: "Comments",
+                column: "BlogID",
+                principalTable: "Blogs",
+                principalColumn: "BlogID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Blogs_BlogID",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_BlogID",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "BlogID",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryID",
+                table: "Blogs");
+        }
+    }
+}
