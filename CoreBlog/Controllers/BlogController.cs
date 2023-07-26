4066693353fdc38002d3689d84e7160e5f3092ae
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-07-26 23:24:52 +0300

    some changings on tema and blog listing

--- CoreBlog/Controllers/BlogController.cs
+++ CoreBlog/Controllers/BlogController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+        public IActionResult BlogReadAll(int id) 
+        {
+            var values =bm.GetBlogByID(id);
+            return View(values);
         }
     }
 }
