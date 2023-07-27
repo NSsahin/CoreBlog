b8296900520e0d8233cbd8fa8e58e71d110d918b
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-07-27 21:21:09 +0300

    user comments and view component usage

---
+++ CoreBlog/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment> 
+            { 
+                new UserComment
+                {
+                    ID=1,
+                    Username="Murat"
+                },
+                new UserComment 
+                {
+                    ID=2,
+                    Username="Mesut"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Merve"
+                }
+            };
+
+            return View(commentvalues);
+        }
+    }
+}
