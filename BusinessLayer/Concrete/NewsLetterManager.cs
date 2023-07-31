4fd17e6a1a6833c6615a7b69a2ca6a8f8003b8b8
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-07-31 03:23:47 +0300

    about/contact showcase, news subs and error page

---
+++ BusinessLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class NewsLetterManager : INewsLetterService
+    {
+        INewsLetterDal _newsletterdal;
+
+        public NewsLetterManager(INewsLetterDal newsletterdal)
+        {
+            _newsletterdal = newsletterdal;
+        }
+
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newsletterdal.Insert(newsLetter);
+        }
+    }
+}
--- CoreBlog/Controllers/CommentController.cs
+++ CoreBlog/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
@@ -11,10 +12,22 @@ namespace CoreBlog.Controllers
         {
             return View();
         }
+
+        [HttpGet]
         public PartialViewResult PartialAddComment()
         {
             return PartialView();
         }
+        [HttpPost]
+        public PartialViewResult PartialAddComment(Comment p)
+        {
+            p.CommentDate=DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            p.BlogID = 2;
+            cm.CommentAdd(p);
+            return PartialView();
+        }
+
         public PartialViewResult CommentListByBlog(int id)
         {
             var values=cm.GetList(id);
