b1f3fa3121d0e447eec5645167a8f3c9769d8067
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-08-07 00:14:35 +0300

    admin page, areas, admin category page

--- CoreBlog/Program.cs
+++ CoreBlog/Program.cs
@@ -11,19 +11,19 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddMvc(config =>
 {
-	var policy = new AuthorizationPolicyBuilder()
-		.RequireAuthenticatedUser()
-		.Build();
-	config.Filters.Add(new AuthorizeFilter(policy));
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
 });
 
 builder.Services.AddMvc();
 builder.Services.AddAuthentication(
-	CookieAuthenticationDefaults.AuthenticationScheme)
-	.AddCookie(x =>
-	{
-		x.LoginPath = "/Login/Index";
-	}
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x =>
+    {
+        x.LoginPath = "/Login/Index";
+    }
 );
 
 
@@ -33,9 +33,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
@@ -50,8 +50,20 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+        );
+    endpoints.MapControllerRoute(
+      name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+});
 
 app.Run();
