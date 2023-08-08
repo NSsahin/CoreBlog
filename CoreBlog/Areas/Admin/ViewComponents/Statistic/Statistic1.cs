2cb3629ca22b4a0966ae6807f2cceee659f9ba14
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-08-09 00:06:12 +0300

    admin table and static google chart example

--- CoreBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreBlog.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,12 @@ namespace CoreBlog.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "76eae218bb2415efb7afe09089f214a9";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Adana&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
--- DataAccessLayer/Migrations/20230808182946_mig_admintable_add.Designer.cs
+++ DataAccessLayer/Migrations/20230808182946_mig_admintable_add.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230808182946_mig_admintable_add")]
+    partial class mig_admintable_add
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
