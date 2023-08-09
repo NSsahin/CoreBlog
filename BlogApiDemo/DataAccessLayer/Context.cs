fa03e142acaac7ae48820682b88d724edf2876c6
NSsahin <16008119018@ogr.bozok.edu.tr>
2023-08-10 00:02:28 +0300

    ajax and api usage

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-654A43A\\SQLEXPRESS;database=CoreBlogApiDb;integrated Security=true;Encrypt=False");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
